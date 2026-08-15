24da649110f0795c89ab6dea515b443e40a22f76
FraXy <ruzovymotylci@gmail.com>
2026-08-16 00:39:59 +0200

    Extract FraXy competition country quiz data

---
+++ games/fraxy-soutez/data.js
@@ -0,0 +1,197 @@
+export const countries = {
+    Albania: {
+        coords: [41.1533, 20.1683],
+        question: 'Kolik litrů rakií průměrně vypije Albánec za rok?',
+        answer: 5
+    },
+    Andorra: {
+        coords: [42.5078, 1.5211],
+        question: 'Kolik turistů navštíví Andorru ročně?',
+        answer: 10000000
+    },
+    Belgium: {
+        coords: [50.8503, 4.3517],
+        question: 'Kolik kilogramů čokolády průměrně sní Belgičan za rok?',
+        answer: 8
+    },
+    'Bosnia and Herzegovina': {
+        coords: [43.9159, 17.6791],
+        question: 'Kolik tun kávy se zde ročně spotřebuje?',
+        answer: 15000
+    },
+    Bulgaria: {
+        coords: [42.7339, 25.4858],
+        question: 'Kolik tun růžového oleje Bulharsko vyprodukuje ročně?',
+        answer: 2
+    },
+    'Czech Republic': {
+        coords: [49.8175, 15.473],
+        question: 'Kolik litrů piva průměrně vypije Čech za rok?',
+        answer: 140
+    },
+    Montenegro: {
+        coords: [42.7083, 19.3744],
+        question: 'Kolik kilometrů pobřeží má Černá Hora?',
+        answer: 293
+    },
+    Denmark: {
+        coords: [56.2639, 9.5018],
+        question: 'Kolik ostrovů patří Dánsku?',
+        answer: 443
+    },
+    Estonia: {
+        coords: [58.5953, 25.0136],
+        question: 'Kolik procent Estonců používá internet denně? - Nepiš znaménko %',
+        answer: 90
+    },
+    Finland: {
+        coords: [61.9241, 25.7482],
+        question: 'Kolik saun se nachází ve Finsku?',
+        answer: 2000000
+    },
+    France: {
+        coords: [46.6034, 1.8883],
+        question: 'Kolik druhů sýrů se ve Francii vyrábí?',
+        answer: 1200
+    },
+    Croatia: {
+        coords: [45.1, 15.2],
+        question: 'Kolik turistů navštíví Chorvatsko ročně?',
+        answer: 20000000
+    },
+    Ireland: {
+        coords: [53.1424, -7.6921],
+        question: 'Kolik litrů whiskey vyprodukuje Irsko ročně?',
+        answer: 160000000
+    },
+    Iceland: {
+        coords: [64.9631, -19.0208],
+        question: 'Kolik geotermálních pramenů je na Islandu?',
+        answer: 250
+    },
+    Italy: {
+        coords: [41.8719, 12.5674],
+        question: 'Kolik pizz se sní v Itálii za den?',
+        answer: 1000000
+    },
+    Kosovo: {
+        coords: [42.6026, 20.903],
+        question: 'Kolik procent obyvatel Kosova tvoří mladí lidé do 30 let? - Nepiš znaménko %',
+        answer: 65
+    },
+    Liechtenstein: {
+        coords: [47.166, 9.5554],
+        question: 'Kolik obyvatel má Lichtenštejnsko?',
+        answer: 39000
+    },
+    Lithuania: {
+        coords: [55.1694, 23.8813],
+        question: 'Kolik hektarů lesa pokrývá Litvu?',
+        answer: 2000000
+    },
+    Latvia: {
+        coords: [56.8796, 24.6036],
+        question: 'Kolik písní je zaznamenáno v Lotyšském národním zpěvníku?',
+        answer: 1200000
+    },
+    Luxembourg: {
+        coords: [49.8153, 6.1296],
+        question: 'Kolik bank má Lucembursko?',
+        answer: 120
+    },
+    Hungary: {
+        coords: [47.1625, 19.5033],
+        question: 'Kolik termálních pramenů je v Maďarsku?',
+        answer: 1300
+    },
+    Malta: {
+        coords: [35.8997, 14.5146],
+        question: 'Kolik turistů navštíví Maltu ročně?',
+        answer: 2500000
+    },
+    Moldova: {
+        coords: [47.4116, 28.3699],
+        question: 'Kolik litrů vína průměrně vyprodukuje Moldavsko ročně?',
+        answer: 120000000
+    },
+    Monaco: {
+        coords: [43.7384, 7.4246],
+        question: 'Kolik kilometrů čtverečních zabírá Monako?',
+        answer: 2
+    },
+    Netherlands: {
+        coords: [52.1326, 5.2916],
+        question: 'Kolik větrných mlýnů je v Nizozemsku?',
+        answer: 1200
+    },
+    Norway: {
+        coords: [60.472, 8.4689],
+        question: 'Kolik fjordů má Norsko?',
+        answer: 1190
+    },
+    Poland: {
+        coords: [51.9194, 19.1451],
+        question: 'Kolik tun jablek Polsko vyprodukuje ročně?',
+        answer: 4000000
+    },
+    Portugal: {
+        coords: [39.3999, -8.2245],
+        question: 'Kolik lahví portského vína vyprodukuje Portugalsko ročně?',
+        answer: 6000000
+    },
+    Austria: {
+        coords: [47.5162, 14.5501],
+        question: 'Kolik procent Rakouska pokrývají Alpy? - Nepiš znaménko %',
+        answer: 62
+    },
+    Romania: {
+        coords: [45.9432, 24.9668],
+        question: 'Kolik medvědů žije v Karpatech?',
+        answer: 6000
+    },
+    Greece: {
+        coords: [39.0742, 21.8243],
+        question: 'Kolik ostrovů tvoří Řecko?',
+        answer: 6000
+    },
+    'San Marino': {
+        coords: [43.9336, 12.4508],
+        question: 'Kolik obyvatel má San Marino?',
+        answer: 34000
+    },
+    Slovakia: {
+        coords: [48.669, 19.699],
+        question: 'Kolik hradů a zámků má Slovensko?',
+        answer: 425
+    },
+    Slovenia: {
+        coords: [46.1512, 14.9955],
+        question: 'Kolik jeskyní je ve Slovinsku?',
+        answer: 10000
+    },
+    Spain: {
+        coords: [40.4637, -3.7492],
+        question: 'Kolik olivovníků roste ve Španělsku?',
+        answer: 300000000
+    },
+    Sweden: {
+        coords: [60.1282, 18.6435],
+        question: 'Kolik jezer se nachází ve Švédsku?',
+        answer: 100000
+    },
+    Switzerland: {
+        coords: [46.8182, 8.2275],
+        question: 'Kolik hodin ročně průměrně Švýcaři pracují?',
+        answer: 1600
+    },
+    Ukraine: {
+        coords: [48.3794, 31.1656],
+        question: 'Kolik kilometrů hranic má Ukrajina?',
+        answer: 6500
+    },
+    'United Kingdom': {
+        coords: [55.3781, -3.436],
+        question: 'Kolik šálků čaje se vypije ve Velké Británii za den?',
+        answer: 165000000
+    }
+};
